767cfffc7393fb39030024a97149a0fcfd6eef25
Laure Maffini <lauremaffini@gmail.com>
2024-11-12 12:40:35 +0100

    [feat] : layout desktop ok - mq responsive mobile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,9 @@ import "./App.css";
 import Game_page from "./pages/Game_page";
 
 function App() {
-  return <Game_page />;
+  return (
+  <Game_page />
+  )
 }
 
 export default App;
--- client/src/components/Game_zone/Game_zone.tsx
+++ client/src/components/Game_zone/Game_zone.tsx
@@ -1,5 +1,33 @@
+import "./Game_zone.css";
+
 function Game_zone() {
-  return "Game_Zone";
+  return (
+    <main>
+      <section id="zoneDeJeu">
+        <div className="zoneDePiochePlayer" />
+        <section id="player">
+          <div className="zoneDeJeuPlayer">
+            <img
+              src="../src/assets/images/Card_Placeholder.png"
+              alt="rectangle blanc transparent"
+            />
+            <p>Pseudo</p>
+          </div>        
+        </section>
+        
+        <section id="computer">
+          <div className="zoneDeJeuComputer">
+            <img
+              src="../src/assets/images/Card_Placeholder.png"
+              alt="rectangle blanc transparent"
+            />
+            <p>Ordinateur</p>
+          </div>        
+        </section>
+        <div className="zoneDePiocheComputer" />
+      </section>
+    </main>
+  );
 }
 
 export default Game_zone;
