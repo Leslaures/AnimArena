408180840a48b2c3f2d2199230b6e8bb62c7a704
MateuszPlebanek <matprocom@gmail.com>
2024-11-13 10:53:50 +0100

    [fix]: div remove in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,7 @@ import "./App.css";
 import Game_page from "./pages/Game_page";
 
 function App() {
-  return (
-    <div className="App">
-      <Game_page />
-    </div>
-  );
+  return <Game_page />;
 }
 
 export default App;
