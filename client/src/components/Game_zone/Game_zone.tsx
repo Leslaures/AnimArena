8a0f3e751c355bde792b8801c398bcadda147239
Laure Maffini <lauremaffini@gmail.com>
2024-11-13 09:28:25 +0100

    [feat] : responsive mobile ok

--- client/src/components/Game_zone/Game_zone.tsx
+++ client/src/components/Game_zone/Game_zone.tsx
@@ -5,25 +5,31 @@ function Game_zone() {
     <main>
       <section id="zoneDeJeu">
         <div className="zoneDePiochePlayer" />
+
         <section id="player">
           <div className="zoneDeJeuPlayer">
-            <img
-              src="../src/assets/images/Card_Placeholder.png"
-              alt="rectangle blanc transparent"
-            />
+            <div id="imgPlayerContainer">
+              <img
+                src="../src/assets/images/Card_Placeholder.png"
+                alt="rectangle blanc transparent"
+              />
+            </div>
             <p>Pseudo</p>
           </div>
         </section>
 
         <section id="computer">
           <div className="zoneDeJeuComputer">
-            <img
-              src="../src/assets/images/Card_Placeholder.png"
-              alt="rectangle blanc transparent"
-            />
+            <div id="imgComputerContainer">
+              <img
+                src="../src/assets/images/Card_Placeholder.png"
+                alt="rectangle blanc transparent"
+              />
+            </div>
             <p>Ordinateur</p>
           </div>
         </section>
+
         <div className="zoneDePiocheComputer" />
       </section>
     </main>
